5a2c88f8d937032a7c58f1c51d998e1ce20a19ba
dogfrogfog <maksim.hodasevich@gmail.com>
2022-07-31 16:57:13 +0200

    feat: add router (react-location)

--- minting-dapp-admin/src/index.tsx
+++ minting-dapp-admin/src/index.tsx
@@ -1,16 +1,37 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ReactLocationDevtools } from '@tanstack/react-location-devtools';
 
+import {
+  Link,
+  Outlet,
+  ReactLocation,
+  Router,
+  Route,
+  DefaultGenerics,
+} from "@tanstack/react-location";
+
+import { ROUTES } from './constants';
 import Test from './pages/Test';
-import './global.scss';
+import Test2 from './pages/Test2';
 
 const container = document.getElementById('root');
-
 const root = createRoot(container!);
+
+const location = new ReactLocation();
+const routes: Route<DefaultGenerics>[] = [
+  { path: ROUTES.home, element: <Test /> },
+  { path: ROUTES.nftMintRequest, element: <Test2 /> },
+];
+
 root.render(
   <StrictMode>
-    <div className="global">
-      <Test />
-    </div>
+    <Router location={location} routes={routes}>
+      <Link to={ROUTES.home} activeOptions={{ exact: true }}>/home</Link>
+      <br />
+      <Link to={ROUTES.nftMintRequest} activeOptions={{ exact: true }}>/nft-mint-request</Link>
+      <Outlet />
+      {process.env.NODE_ENV === 'development' ? <ReactLocationDevtools /> : null}
+    </Router>
   </StrictMode>
 );

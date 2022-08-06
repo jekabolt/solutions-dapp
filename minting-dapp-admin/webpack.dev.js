571cf770ca8f548df5ac2daa2c7c26986a03dac8
jekabolt <jekabolt@yahoo.com>
2022-08-06 13:04:23 +0300

    conflict fix

--- minting-dapp-admin/webpack.dev.js
+++ minting-dapp-admin/webpack.dev.js
@@ -1,22 +1,19 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public/'),
+    mode: 'development',
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public/'),
+        },
+        compress: true,
+        port: 4040,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:4040',
+                router: () => 'http://localhost:3999',
+            }
+        },
     },
-    compress: true,
-    port: 4040,
-<<<<<<< HEAD
-=======
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4040',
-        router: () => 'http://localhost:3999',
-      }
-    },
->>>>>>> 910ce0f57929e011abf7ac9af44dd93f7c3ef56c
-  },
 };

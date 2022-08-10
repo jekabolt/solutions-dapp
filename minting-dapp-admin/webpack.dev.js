b5cb6044e438cabc94d34d4ced5dc4cfe551b674
dogfrogfog <maksim.hodasevich@gmail.com>
2022-08-10 22:37:30 +0200

    fix: cleanup

--- minting-dapp-admin/webpack.dev.js
+++ minting-dapp-admin/webpack.dev.js
@@ -1,19 +1,19 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'public/'),
-        },
-        compress: true,
-        port: 4040,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:4040',
-                router: () => 'http://localhost:3999',
-            }
-        },
-    },
+	mode: 'development',
+	devtool: 'source-map',
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'public/'),
+		},
+		compress: true,
+		port: 4040,
+		proxy: {
+			'/api': {
+				target: 'http://localhost:4040',
+				router: () => 'http://localhost:3999',
+			}
+		},
+	},
 };

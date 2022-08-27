1fee9a5c5c37901c18ce8a501e8b072efd70b213
dogfrogfog <maksim.hodasevich@gmail.com>
2022-08-27 18:16:31 +0300

    feat: add redirect to auth page
    * redirect if there is no token in localstorage
    * redirect if token is invalid (/api/nft/requests returns 401 status)

--- admin-panel/src/pages/Home.tsx
+++ admin-panel/src/pages/Home.tsx
@@ -1,46 +1,42 @@
-import { FC, Fragment } from 'react';
+import { FC, useEffect } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import { Link } from '@tanstack/react-location';
+import { useNavigate } from '@tanstack/react-location';
 
 import { getNftRequests, QUERIES } from 'api';
-import { NftPreview } from 'components/NftPreview';
 import { ROUTES } from 'constants/routes';
+import { AUTH_LOCAL_STORAGE_KEY } from 'constants/values';
+import { NftPreview } from 'components/NftPreview';
 
 import styles from 'styles/nft-list-page.module.scss';
 
 export const Home: FC = () => {
-  const { data } = useQuery([QUERIES.getNftRequests], () =>
-    getNftRequests(localStorage.getItem('authToken') || '')
+  const token = localStorage.getItem(AUTH_LOCAL_STORAGE_KEY);
+  const navigate = useNavigate();
+  // fix request on every render...
+  // add types..import from proto and use generics
+  const { data, error, refetch } = useQuery([QUERIES.getNftRequests], () =>
+    getNftRequests(token || ''),
+    { enabled: false, retry: false },
   );
+
+  console.log(data);
+
+  useEffect(() => {
+    // redirect to auth if there is no token in LS or token is invalid
+    //@ts-ignore
+    if (!token || error?.response?.status === 401) {
+      navigate({ to: ROUTES.auth });
+    } else {
+      refetch();
+    }
+    // @ts-ignore
+  }, [token, error?.response?.status]);
 
   return (
     <div className={styles.container}>
       home page
-      <br />
-      <br />
-      <br />
-      <br />
-      <Link to={ROUTES.auth} activeOptions={{ exact: true }}>
-        /auth
-      </Link>
-      {/* <div className={styles.pageHeader}>
-        <h3>some sort of data</h3>
-      </div>
-      <div className={styles.nftList}>
-        <div style={{ backgroundColor: localStorage.getItem('authToken') ? 'lightgreen' : 'red' }}>auth</div>
-        <br />
-        <br />
-        <div style={{ backgroundColor: data?.nftMintRequests ? 'lightgreen' : 'red' }}>
-          getNftRequests
-        </div>
-        {data?.nftMintRequests &&
-          data.nftMintRequests.map((nftRequest: any, index: number) => (
-            // change key
-            <Fragment key={index}>
-              <NftPreview />
-            </Fragment>
-          ))}
-      </div> */}
+
+      <button onClick={() => localStorage.setItem(AUTH_LOCAL_STORAGE_KEY, '')}>clear localst</button>
     </div>
   );
 };
